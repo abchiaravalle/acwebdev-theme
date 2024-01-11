27595f99c08d6c660d5b88a7400255f9303abe5e
Adam Chiaravalle <adamchiaravalle@gmail.com>
2024-01-11 14:16:18 -0500

    init

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Beginning of footer.php -->
+
+</body>
+
+<footer>
+    <!-- Footer content goes here -->
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+    <!-- Header content goes here, like site title or logo -->
+</header>
+
+<!-- Rest of your theme's structure starts here -->
+
+<body>
